819a1ac4915df243fb6a1700e7395962f9decc86
Carson King <122931138+Carson301@users.noreply.github.com>
2024-10-29 11:20:03 -0400

    Added commented out code to test the views I implemented.

--- Main.py
+++ Main.py
@@ -1,9 +1,36 @@
 import arcade
 from GUI.Window import Window
+from GameStates.GameInfo import GameInfo
+from GameStates.PickCardView import PickCardView
+from GameStates.AddToCribView import AddToCribView
+from GameStates.CutDeckView import CutDeckView
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Super Cool Cribbage"
+
 
 def main():
-    window = Window()
-    window.setup()
+    # window = Window()
+    # window.setup()
+    # arcade.run()
+
+    # Creates a window for the cribbage game to be displayed on
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+    # Create a game_state object
+    game_info = GameInfo()
+
+    # pick_card_view = PickCardView(game_info)
+    # window.show_view(pick_card_view)
+
+    # add_crib_view = AddToCribView(game_info)
+    # window.show_view(add_crib_view)
+
+    # cut_deck_view = CutDeckView(game_info)
+    # window.show_view(cut_deck_view)
+
+    # Run the Window
     arcade.run()
 
 if __name__ == '__main__':
