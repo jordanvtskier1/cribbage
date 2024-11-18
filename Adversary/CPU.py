ef0b0a8fd0f434bbe9c7da27bc98b35cd2871081
Kate O'Brien <kate.obrien@uvm,edu>
2024-11-18 14:45:53 -0500

    cleaned up CPU

--- Adversary/CPU.py
+++ Adversary/CPU.py
@@ -19,25 +19,19 @@ class CPU(OtherPlayerLogic):
         return opponent_card
 
     def add_to_cribbage(self, game_info: GameInfo):
-        card_indexes = [
-            random.randint(0, len(game_info.other_hand)-1),
-            random.randint(0, len(game_info.other_hand)-2)
-        ]
-        # I know its ugly im sorry
-        if card_indexes[0] == card_indexes[1]:
-            card_indexes[1] = (card_indexes[1] + random.randint(1, 3)) % 4
+        card_indexes = random.sample(range(len(game_info.other_hand)), 2)
         return [game_info.other_hand[card_indexes[0]], game_info.other_hand[card_indexes[1]]]
 
     def cut_deck(self, game_info: GameInfo):
+        # Randomly select a card from the deck
         deck_size = len(game_info.deck)
-
         card_index = random.randint(0, deck_size - 1)
+        cut_card = game_info.deck[card_index]  
 
-        card = game_info.deck[card_index]
-        return card
+        print(f"CPU cut card: {cut_card}")
+        return cut_card
 
     def play_card(self, game_info: GameInfo):
         card_index = random.randint(0, len(game_info.other_hand) - 1)
         card = game_info.other_hand[card_index]
-
         return card
