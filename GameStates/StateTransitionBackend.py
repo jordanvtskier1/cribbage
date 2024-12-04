69f4b383ce2bb117d67e37b65237110bb7840a0c
jordanvtskier1 <jmgottli@uvm.edu>
2024-12-04 12:34:38 -0500

    Fixed hand scores

--- GameStates/StateTransitionBackend.py
+++ GameStates/StateTransitionBackend.py
@@ -277,6 +277,9 @@ class StateTransitionBackend:
     def play_to_show_score(self, game_info: GameInfo):
         from GameStates.ActiveViews.ShowScoreView import ShowScoreView
 
+        game_info.our_score += game_info.our_hand_score
+        game_info.other_score += game_info.other_hand_score
+
         crib_score = Backend.calculate_crib_score(game_info)
         if game_info.is_dealer:
             game_info.our_score += crib_score
